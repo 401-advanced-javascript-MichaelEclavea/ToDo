6b699eadb02e57a3550c31018185e1288f453371
Michael Eclavea <usermike671@gmail.com>
2020-11-24 23:59:20 -0800

    added styling

--- src/components/todo.js
+++ src/components/todo.js
@@ -5,10 +5,16 @@ import TodoList from './list.js';
 import './todo.scss';
 
 function ToDo (props) {
+  const [id, setId] = useState(0);
+  const [list, setList] = useState([
+    { _id: 1, complete: false, text: 'Clean the Kitchen', difficulty: 3, assignee: 'Person A'},
+    { _id: 2, complete: false, text: 'Do the Laundry', difficulty: 2, assignee: 'Person A'},
+    { _id: 3, complete: false, text: 'Walk the Dog', difficulty: 4, assignee: 'Person B'},
+    { _id: 4, complete: true, text: 'Do Homework', difficulty: 3, assignee: 'Person C'},
+    { _id: 5, complete: false, text: 'Take a Nap', difficulty: 1, assignee: 'Person B'}
+  ]);
 
-  const [list, setList] = useState([]);
-
-
+  
   function addItem(item){
     item._id = Math.random();
     item.complete = false;
@@ -17,43 +23,18 @@ function ToDo (props) {
       });
   };
 
-  function toggleComplete(id){
-
-    setList(prevItem => {
-      return prevItem.filter((item, index) => {
-        return index !== id;
-      });
-    });
-
+  function toggleComplete(id) {
     let item = list.filter(i => i._id === id)[0] || {};
-
     if (item._id) {
       item.complete = !item.complete;
-      let list = list.map(listItem => listItem._id === item._id ? item : listItem);
-      setList({list});
+      let map = list.map(listItem => listItem._id === item._id ? item : listItem);
+      setList(map);
     }
-
-  };
-
-  // componentDidMount() {
-  //   let list = [
-  //     { _id: 1, complete: false, text: 'Clean the Kitchen', difficulty: 3, assignee: 'Person A'},
-  //     { _id: 2, complete: false, text: 'Do the Laundry', difficulty: 2, assignee: 'Person A'},
-  //     { _id: 3, complete: false, text: 'Walk the Dog', difficulty: 4, assignee: 'Person B'},
-  //     { _id: 4, complete: true, text: 'Do Homework', difficulty: 3, assignee: 'Person C'},
-  //     { _id: 5, complete: false, text: 'Take a Nap', difficulty: 1, assignee: 'Person B'},
-  //   ];
-
-  //   setList({list});
-  // }
-
+  }
 
     return (
       <>
         <header>
-          <h2>
-          There are {list.filter(item => !item.complete).length} Items To Complete
-          </h2>
         </header>
 
         <section className="todo">
@@ -62,7 +43,10 @@ function ToDo (props) {
             <TodoForm handleSubmit={addItem} />
           </div>
 
-          <div>
+          <div id="todo-container">
+          <h2>
+          There are {list.filter(item => !item.complete).length} Items To Complete
+          </h2>
             <TodoList
               list={list}
               handleComplete={toggleComplete}
